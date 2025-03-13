e871a275cbd481622b0faa9f659b47cefe73be03
Zeynep Handan Çakır <zeynephandancakir@gmail.com>
2025-03-13 17:36:24 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // Configurazione del database
 $db_host = 'localhost';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp';
+$db_pass = 'hans234ckr';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
